23bab1be20e86002fb52b1d8b199026673d82a4d
Marcelo Rezende Cavalcanti <marcelo@swg2.com.br>
2020-05-12 21:45:20 -0300

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import Constants from 'expo-constants';
+import { Icon } from 'react-native-elements';
 
-
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 
 const MenuNavigator = createStackNavigator({
         Menu: { screen: Menu },
@@ -26,12 +26,45 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff"  
+    })
+});
+
+const MainNavigator = createDrawerNavigator({
+    Home: 
+      { screen: HomeNavigator,
+        navigationOptions: {
+          title: 'Home',
+          drawerLabel: 'Home'
+        }
+      },
+    Menu: 
+      { screen: MenuNavigator,
+        navigationOptions: {
+          title: 'Menu',
+          drawerLabel: 'Menu'
+        }, 
+      }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
+
 class Main extends Component {
   
 	render() {
 		return (
 			<View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-				<MenuNavigator />
+				<MainNavigator />
 			</View>
 		);
 	};
