591cf9050bf61fe5f98ccdd61962fbd7fd4e898f
Marcelo Rezende Cavalcanti <marcelo@swg2.com.br>
2020-05-19 13:45:05 -0300

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -10,6 +10,7 @@ import Home from './HomeComponent';
 import ContactUs from './ContactComponent';
 import AboutUs from './AboutComponent';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
@@ -122,6 +123,23 @@ const ReservationNavigator = createStackNavigator({
     })
 });
 
+const FavoritesNavigator = createStackNavigator({
+    Favorites: { screen: Favorites }
+  }, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+          color: "#fff"            
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        color= 'white'
+        onPress={ () => navigation.toggleDrawer() } />
+    })
+});
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -194,6 +212,21 @@ const MainNavigator = createDrawerNavigator({
 				name='address-card'
 				type='font-awesome'            
 				size={22}
+				color={tintColor}
+			  />
+			),
+        }, 
+      },
+    Favorites: 
+      { screen: FavoritesNavigator,
+        navigationOptions: {
+			title: 'My Favorites',
+			drawerLabel: 'My Favorites',
+			drawerIcon: ({ tintColor, focused }) => (
+			  <Icon
+				name='heart'
+				type='font-awesome'            
+				size={24}
 				color={tintColor}
 			  />
 			),
